f9fb612689f35bd2bf01640bcaa39836217d30a0
HubenerH10 <hubener.ramos@rebic.com.br>
2026-09-25 15:49:50 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Cole aqui a URL /exec da implantação do Apps Script (veja LEIA-ME.md)
+// Cole aqui a URL /exec da implantação do Apps Script
 window.PORTAL_API = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
