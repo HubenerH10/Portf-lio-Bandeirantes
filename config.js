856b7bc0c9dca48d403d627557654a80de676558
HubenerH10 <hubener.ramos@rebic.com.br>
2026-09-25 16:32:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Cole aqui a URL /exec da implantação do Apps Script
-window.PORTAL_API = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+// URL da implantação do Apps Script
+window.PORTAL_API = "https://script.google.com/macros/s/AKfycbwS9-Hp9rlWu3rwcrkyV6a4s0buKhwG3P7DolxOLCTAS3ZXO4ZhCyMJDoya8sUd2FlUIg/exec";
